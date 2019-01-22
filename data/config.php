06466d1e664b2e576bb4858eaed7cffbe6294efa
Patbox <39821509+Patbox@users.noreply.github.com>
2019-01-22 19:39:41 +0100

    Update config.php

--- data/config.php
+++ data/config.php
@@ -2,14 +2,14 @@
 // Tutaj wpisujesz nazwę strony.
 $nazwa = "Simple News";
 // Tutaj podajesz adres strony.
-$path="/publiczne/strony/boxcms/";
+$path="/";
 //Hasło potrzebne do utworzenia newsa.
 $haslo = "password";
 
 // W tej wartości podajesz link.
 $link[0]="https://patbox.ml";
 // W tej wartości podajesz nazwę tego link.
-$link_nazwa[0]="Twórca";
+$link_nazwa[0]="Strona twórcy";
 
 // Aby dodać kolejne linki należy skopiować wartości powyżej (poprzedzonych // nie trzeba),
 // a potem zwimienić liczbę w [] na jedną wiąkszą, czyli 0 na 1, 1 na 2 itd.
